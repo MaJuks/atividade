48df32aaf2fd84c858ebe551a58539ea5ed6555d
Maria Júlia <75435563+MaJuks@users.noreply.github.com>
2025-05-12 15:00:39 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/atividade/',
+    base: '/estilizacao-react/',
   plugins: [react(),tailwindcss(),],
 })
